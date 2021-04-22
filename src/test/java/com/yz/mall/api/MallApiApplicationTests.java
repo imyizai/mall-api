b1d5f467a17ff8cde95c5cb198d4bfbff898866c
刘毅 <imyizai@promote.cache-dns.local>
2021-04-22 16:01:00 +0800

    楼楼商城api项目初始化

---
+++ src/test/java/com/yz/mall/api/MallApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yz.mall.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
